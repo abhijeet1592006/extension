7ea2bead0faeca19e2163af96200ac2e3fd01622
abhijeet1592006 <abhijeet8800434205@gmail.com>
2025-07-02 17:25:25 +0530

    update

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-heading=document.getElementById("heading")
-content=document.getElementById("content")
-btn=document.getElementById("btn")
-heading.style.display="none"
-content.style.display="none"
-btn.style.display="none"
-plus=document.getElementById("new")
+heading = document.getElementById("heading")
+content = document.getElementById("content")
+btn = document.getElementById("btn")
+heading.style.display = "none"
+content.style.display = "none"
+btn.style.display = "none"
+plus = document.getElementById("new")
+homepage = document.getElementById("homepage")
 
 
-plus.addEventListener("click",function write(){
-    document.getElementById("homepage").style.display="none"
-    document.getElementById("heading").style.display="grid"
-    document.getElementById("content").style.display="grid"
-    document.getElementById("btn").style.display="grid"
+plus.addEventListener("click", function write() {
+    document.getElementById("homepage").style.display = "none"
+    document.getElementById("heading").style.display = "grid"
+    document.getElementById("content").style.display = "grid"
+    document.getElementById("btn").style.display = "grid"
 })
+
+title=document.createElement("div")
+title.textContent = "hi"
+title.classList.add("card")
+
+homepage.appendChild(title);         
+
+
+j=()=>{document.getElementById("s").style.display="none"}
+function save(){
+
+    
+    hc=(heading.innerText)
+    c=content.innerText
+
+    localStorage.setItem(hc,c)
+
+    document.getElementById("s").style.display="grid"
+    setTimeout(j,1000)
+}
+
+function load(id){
+    heading.innerText=id
+
+    nc=localStorage.getItem(id)
+
+    content.innerText=nc
+}
