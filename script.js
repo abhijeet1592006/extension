a5afba536f780345e4fe5ee2ffe209bde1d22228
abhijeet1592006 <abhijeet8800434205@gmail.com>
2025-07-02 17:57:15 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+
+
+cd=document.getElementById("card")
 heading = document.getElementById("heading")
 content = document.getElementById("content")
 btn = document.getElementById("btn")
@@ -7,6 +11,9 @@ btn.style.display = "none"
 plus = document.getElementById("new")
 homepage = document.getElementById("homepage")
 
+btn.addEventListener("click",function(){
+    save()
+})
 
 plus.addEventListener("click", function write() {
     document.getElementById("homepage").style.display = "none"
@@ -15,30 +22,64 @@ plus.addEventListener("click", function write() {
     document.getElementById("btn").style.display = "grid"
 })
 
-title=document.createElement("div")
-title.textContent = "hi"
-title.classList.add("card")
 
-homepage.appendChild(title);         
+function t() {
 
+    l=Object.keys(localStorage)
 
-j=()=>{document.getElementById("s").style.display="none"}
-function save(){
+    for(i of l){
+        title=document.createElement("div")
+        title.textContent=i;
+        title.classList.add("card")
+        homepage.append(title);
 
+    }
+    let d=document.querySelectorAll(".card")
+    d.forEach(d1 => {
+        d1.addEventListener("click", function write() {
+            document.getElementById("homepage").style.display = "none"
+            document.getElementById("heading").style.display = "grid"
+            document.getElementById("content").style.display = "grid"
+            document.getElementById("btn").style.display = "grid"
+            id=d1.innerText
+            load(id)
+        })
+        
+    });
     
-    hc=(heading.innerText)
-    c=content.innerText
 
-    localStorage.setItem(hc,c)
 
-    document.getElementById("s").style.display="grid"
-    setTimeout(j,1000)
+
 }
 
-function load(id){
-    heading.innerText=id
 
-    nc=localStorage.getItem(id)
 
-    content.innerText=nc
+j = () => { document.getElementById("s").style.display = "none" }
+function save() {
+
+
+    hc = (heading.innerText)
+    c = content.innerText
+
+    localStorage.setItem(hc, c)
+
+    document.getElementById("s").style.display = "grid"
+    setTimeout(j, 1000)
 }
+
+function load(id) {
+    heading.innerText = id
+
+    nc = localStorage.getItem(id)
+
+    content.innerText = nc
+}
+
+
+cd.addEventListener("click",function(){
+    
+    id=card.innerText
+    load(id)
+})
+t()
+
